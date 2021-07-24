98eb57bb752a4d50f5dfa665bb3acb7761561800
Rafael Ivan Hattge <rafaelhattge@gmail.com>
2021-07-24 00:48:53 -0300

    Added Modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,70 @@
 const coursesEl = document.querySelector(".courses");
 const hamburger = document.querySelector(".hamburger");
+const modalEl = document.querySelector(".modal");
+const modalButtonEl = document.querySelector(".modal__button");
+const modalContainerEl = document.querySelector(".modal__container");
 let courses;
 
 // AJAX
 const getCourses = async () => {
     const data = await (await fetch("data/data.json")).json();
     courses = data.cursos;
+    courses.map((item, index) => {
+        item.id = index;
+    })
     renderCourses(courses);
 }
 
 const renderCourses = data => {
     coursesEl.innerHTML = "";
-    data.map(cursos => { coursesEl.insertAdjacentHTML("afterbegin", `<div class="course">${cursos.curso.titulo}</div>`) });
+    data.map((item, index) => {
+        coursesEl.insertAdjacentHTML("beforeend", `<div data-id=${item.id} class="course">${item.curso.titulo}</div>`)
+    });
+    document.querySelectorAll('.course').forEach(item => {
+        item.addEventListener('click', e => {
+            document.querySelector('.modal').classList.add("open");
+            renderCourseDetails(e.target.getAttribute("data-id"));
+        });
+    });
 }
+
+const renderCourseDetails = (id) => {
+    const { curso, depoimentos, obras } = courses[id];
+
+    const template = `<div><h2>${curso.titulo}</h2>
+            <p>${curso.tipo}</p>
+            <p>${curso.descricao}</p>
+            ${depoimentos.map(item => {
+        return (
+            `
+            <p>${item.depoimento}</p>
+            <img src=${item.imagem} alt="foto de ${item.nome}" />
+            <p>${item.nome}</p>
+            <p>${item.ocupacao}</p>
+            `
+        )
+    })}`;
+
+    modalContainerEl.innerHTML = "";
+    modalContainerEl.insertAdjacentHTML("beforeend", template);
+}
+
+modalButtonEl.addEventListener("click", () => {
+    modalEl.classList.remove("open");
+});
 
 hamburger.addEventListener("click", () => {
     const ul = document.querySelector(".nav ul");
     ul.classList.contains("open") ? ul.classList.remove("open") : ul.classList.add("open");
     hamburger.classList.contains("open") ? hamburger.classList.remove("open") : hamburger.classList.add("open");
-})
+});
 
-window.addEventListener("DOMContentLoaded", getCourses);
+// document.querySelectorAll('.courses').forEach(item => {
+//     item.addEventListener('click', event => {
+//         //handle click
+//     })
+// })
+
+window.addEventListener("DOMContentLoaded", () => {
+    getCourses();
+});
